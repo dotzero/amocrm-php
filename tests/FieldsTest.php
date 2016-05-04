070306d2ecc362433ea2859eab29b0b154b69444
dotzero <mail@dotzero.ru>
2016-05-04 13:02:10 +0300

    Add test FieldsTest

---
+++ tests/FieldsTest.php
@@ -0,0 +1,63 @@
+<?php
+
+    class FieldsTest extends PHPUnit_Framework_TestCase
+    {
+        private $fields = null;
+
+        public function setUp()
+        {
+            $this->fields = new \AmoCRM\Fields();
+
+            $this->fields->add('key1', 'value1');
+            $this->fields->add('key2', 'value2');
+        }
+
+        public function testAdd()
+        {
+            $this->fields['key3'] = 'value3';
+            $this->fields->key4   = 'value4';
+            $this->fields->add('key5', 'value5');
+            $this->fields->offsetSet('key6', 'value6');
+
+            $this->assertEquals(6, count($this->fields));
+        }
+
+        public function testGet()
+        {
+            $this->assertEquals('value1', $this->fields['key1']);
+            $this->assertEquals('value1', $this->fields->key1);
+            $this->assertEquals('value1', $this->fields->get('key1'));
+            $this->assertEquals('value1', $this->fields->offsetGet('key1'));
+        }
+
+        public function testExists()
+        {
+            $this->assertTrue(isset($this->fields['key1']));
+            $this->assertTrue($this->fields->offsetExists('key1'));
+        }
+
+        public function testUnset()
+        {
+            unset($this->fields['key1']);
+            $this->assertFalse(isset($this->fields['key1']));
+
+            $this->fields->offsetUnset('key2');
+            $this->assertFalse(isset($this->fields['key2']));
+        }
+
+        public function testIterator()
+        {
+            $iterator = $this->fields->getIterator();
+
+            $this->assertEquals('value1', $iterator->current());
+
+            $iterator->next();
+            $this->assertEquals('value2', $iterator->current());
+        }
+
+        public function testCount()
+        {
+            $this->assertEquals(2, count($this->fields));
+            $this->assertEquals(2, $this->fields->count());
+        }
+    }
