284379b62297beeecf56e6454619a522ad98cfd8
dotzero <mail@dotzero.ru>
2016-05-05 17:50:23 +0300

    Add AccountTest

---
+++ tests/AccountTest.php
@@ -0,0 +1,28 @@
+<?php
+
+    class AccountTest extends PHPUnit_Framework_TestCase
+    {
+        private $model = null;
+
+        public function setUp()
+        {
+            $paramsBag   = new \AmoCRM\Request\ParamsBag();
+            $this->model = new \AmoCRM\Models\Account($paramsBag);
+        }
+
+        public function testApiCurrent()
+        {
+            $mock = $this->getMockBuilder('\AmoCRM\Models\Account')
+                ->setConstructorArgs([new \AmoCRM\Request\ParamsBag()])
+                ->setMethods(['apiCurrent'])
+                ->getMock();
+
+            $this->assertInstanceOf('\AmoCRM\Models\Account', $mock);
+
+            $mock->expects($this->atLeastOnce())->method('apiCurrent')
+                ->will($this->returnValue([]));
+
+            $this->assertEquals([], $mock->apiCurrent());
+            $this->assertEquals([], $mock->apiCurrent(true));
+        }
+    }
