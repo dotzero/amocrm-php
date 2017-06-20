28453ff9628770b28a52114fec45f68662273a1b
Abyr Valg <valga@abyrga.ru>
2017-06-20 17:59:08 +0300

    Add test for invalid response with 502 HTTP status code

--- tests/Request/RequestTest.php
+++ tests/Request/RequestTest.php
@@ -223,6 +223,21 @@ class RequestTest extends TestCase
         $this->invokeMethod($this->request, 'parseResponse', [$response, $info]);
     }
 
+    /**
+     * @expectedException \AmoCRM\Exception
+     * @expectedExceptionCode 502
+     * @expectedExceptionMessage Invalid response body.
+     */
+    public function testParseInvalidResponseWithHttpError502()
+    {
+        $response = '<html>Bad Gateway</html>';
+        $info = [
+            'http_code' => 502,
+        ];
+
+        $this->invokeMethod($this->request, 'parseResponse', [$response, $info]);
+    }
+
     public function testPrintDebug()
     {
         $this->request->debug(true);
