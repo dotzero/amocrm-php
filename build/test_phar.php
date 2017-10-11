122bedc6975d18b781c3e7c0ec3de03978f56e7d
dotzero <mail@dotzero.ru>
2017-10-11 22:42:41 +0300

    Update phar

--- build/test_phar.php
+++ build/test_phar.php
@@ -3,17 +3,23 @@ require __DIR__ . '/artifacts/amocrm.phar';
 
 $models = [
     ['account', '\AmoCRM\Models\Account'],
+    ['call', '\AmoCRM\Models\Call'],
+    ['catalog', '\AmoCRM\Models\Catalog'],
+    ['catalog_element', '\AmoCRM\Models\CatalogElement'],
     ['company', '\AmoCRM\Models\Company'],
     ['contact', '\AmoCRM\Models\Contact'],
     ['customer', '\AmoCRM\Models\Customer'],
     ['customers_periods', '\AmoCRM\Models\CustomersPeriods'],
+    ['custom_field', '\AmoCRM\Models\CustomField'],
     ['lead', '\AmoCRM\Models\Lead'],
+    ['links', '\AmoCRM\Models\Links'],
     ['note', '\AmoCRM\Models\Note'],
-    ['task', '\AmoCRM\Models\Task'],
     ['pipelines', '\AmoCRM\Models\Pipelines'],
+    ['task', '\AmoCRM\Models\Task'],
+    ['transaction', '\AmoCRM\Models\Transaction'],
     ['unsorted', '\AmoCRM\Models\Unsorted'],
+    ['webhooks', '\AmoCRM\Models\Webhooks'],
     ['widgets', '\AmoCRM\Models\Widgets'],
-    ['web_hooks', '\AmoCRM\Models\Webhooks'],
 ];
 
 $amo = new \AmoCRM\Client('example', 'login', 'hash');
