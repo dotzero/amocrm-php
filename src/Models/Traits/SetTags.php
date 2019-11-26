ec35f1e523a130dbede77a89710fe30b4d16cc16
Denis Ostrovsky <4deni.kiev@gmail.com>
2019-11-26 17:33:10 +0200

    Able to add tags by id

--- src/Models/Traits/SetTags.php
+++ src/Models/Traits/SetTags.php
@@ -16,7 +16,12 @@ trait SetTags
             $value = [$value];
         }
 
-        $this->values['tags'] = implode(',', $value);
+        $types = array_unique(array_map('gettype', array_values($value)));
+        if (count($types) == 1 && $types[0] == 'integer') {
+            $this->values['tags'] = $value;
+        } else {
+            $this->values['tags'] = implode(',', $value);
+        }
 
         return $this;
     }
